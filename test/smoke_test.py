d305d351999520b7ebbc4556d00180d0b2a24b92
Emanuele Fabbiani <donlelef@gmail.com>
2020-06-02 18:58:35 +0200

    feat: add time series simulation for network

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -1,5 +1,5 @@
 def test_can_import_pandapower_and_pandas():
-    import pandapower.estimation
     import pandas
+    import pandapower.timeseries
     print(f'Pandapower version: {pandapower.__version__}')
     print(f'Pandas version: {pandas.__version__}')
