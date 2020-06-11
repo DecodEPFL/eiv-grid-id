e70315a6d13d717d9cd8b89cdb5168a76ee63be3
Emanuele Fabbiani <donlelef@gmail.com>
2020-06-11 20:46:29 +0200

    feat: introduce new convex optimization solver

--- src/identification/error_metrics.py
+++ src/identification/error_metrics.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import numpy as np
 
 
@@ -5,15 +7,32 @@ def fro_error(y: np.array, y_hat: np.array) -> float:
     return np.linalg.norm(y - y_hat, 'fro')
 
 
-def max_error(y, y_hat):
+def max_error(y: np.array, y_hat: np.array) -> float:
     return np.max(np.abs(y - y_hat))
 
 
-def rrms_error(y, y_hat):
+def rrms_error(y: np.array, y_hat: np.array) -> float:
     return fro_error(y, y_hat) / np.linalg.norm(y, 'fro')
 
 
-def map_error(y, y_hat):
+def map_error(y: np.array, y_hat: np.array) -> float:
     y_non_zero = y[y != 0]
     y_hat_non_zero = y_hat[y != 0]
-    return np.mean(np.abs(y_non_zero - y_hat_non_zero) / np.abs(y_non_zero))
+    return float(np.mean(np.abs(y_non_zero - y_hat_non_zero) / np.abs(y_non_zero)))
+
+
+@dataclass
+class ErrorMetrics:
+    fro_error: float
+    max_error: float
+    rrms_error: float
+    map_error: float
+
+
+def error_metrics(y: np.array, y_hat: np.array) -> ErrorMetrics:
+    return ErrorMetrics(
+        fro_error=fro_error(y, y_hat),
+        max_error=max_error(y, y_hat),
+        rrms_error=rrms_error(y, y_hat),
+        map_error=map_error(y, y_hat)
+    )
--- test/error_metrics_test.py
+++ test/error_metrics_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pytest import fixture
 
-from src.identification.error_metrics import fro_error, max_error, rrms_error, map_error
+from src.identification.error_metrics import fro_error, max_error, rrms_error, map_error, error_metrics
 
 
 @fixture
@@ -28,3 +28,11 @@ def test_rrmse(y, y_hat):
 
 def test_mape(y, y_hat):
     assert map_error(y, y_hat) == 1
+
+
+def test_all_error_metrics(y, y_hat):
+    m = error_metrics(y, y_hat)
+    assert m.fro_error == 2
+    assert m.max_error == np.sqrt(2)
+    assert m.rrms_error == 1
+    assert m.map_error == 1
