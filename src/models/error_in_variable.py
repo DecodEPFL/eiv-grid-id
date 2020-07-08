f1fe6f60fb09b53bbe2be085425032138657e0cd
Emanuele Fabbiani <donlelef@gmail.com>
2020-07-08 19:27:20 +0200

    refactor: add tests for matrix operations

--- src/models/error_in_variable.py
+++ src/models/error_in_variable.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import cvxpy as cp
 import numpy as np
 from tqdm import tqdm
@@ -5,6 +7,13 @@ from tqdm import tqdm
 from src.models.abstract_models import GridIdentificationModel
 from src.models.matrix_operations import make_real_matrix, make_real_vector, vectorize_matrix, make_complex_vector, \
     unvectorize_matrix
+
+
+@dataclass
+class IterationStatus:
+    iteration: int
+    fitted_parameters: np.array
+    target_function: float
 
 
 class TotalLeastSquares(GridIdentificationModel):
@@ -25,14 +34,37 @@ class SparseTotalLeastSquare(GridIdentificationModel):
 
     def __init__(self, lambda_value=10e-2, abs_tol=10e-6, rel_tol=10e-6, max_iterations=50):
         super().__init__()
-        self._lambda_value = lambda_value
+        self._iterations = []
+        self._lambda = lambda_value
         self._abs_tol = abs_tol
         self._rel_tol = rel_tol
         self._max_iterations = max_iterations
 
+    @property
+    def iteration(self):
+        return self._iterations
+
+    @staticmethod
+    def lasso_target(b, A, dA, sigma_b, lambda_value, beta):
+        error = b - (A - dA) @ beta
+        loss = cp.matrix_frac(error, sigma_b) + lambda_value * cp.norm1(beta)
+        return loss
+
+    @staticmethod
+    def qp_target(b, a, da, sigma_b, sigma_a, underline_beta):
+        error = b - underline_beta @ (a - da)
+        loss = cp.matrix_frac(error, sigma_b) + cp.matrix_frac(da, sigma_a)
+        return loss
+
+    @staticmethod
+    def full_target(b, a, da, sigma_b, sigma_a, lambda_value, underline_beta, y):
+        return SparseTotalLeastSquare.qp_target(b, a, da, sigma_b, sigma_a, underline_beta) + lambda_value * cp.norm1(y)
+
+    def is_stationary_point(self, f_cur, f_prev):
+        return np.abs(f_cur - f_prev) < self._abs_tol or np.abs(f_cur - f_prev) / np.abs(f_prev) < self._rel_tol
+
     def fit(self, x: np.array, y: np.array):
-        n = x.shape[1]
-        samples = x.shape[0]
+        samples, n = x.shape
 
         A = make_real_matrix(np.kron(np.eye(n), x))
         dA = make_real_matrix(np.kron(np.eye(n), np.zeros(x.shape)))
@@ -44,50 +76,24 @@ class SparseTotalLeastSquare(GridIdentificationModel):
         beta = cp.Variable(n * n * 2)
         da = cp.Variable(a.size)
 
-        def lasso_target(b, A, dA, sigma_b, lambda_value, beta):
-            error = b - (A - dA) @ beta
-            loss = cp.matrix_frac(error, sigma_b) + lambda_value * cp.norm1(beta)
-            return loss
-
-        def qp_target(b, a, da, sigma_b, sigma_a, underline_beta):
-            error = b - underline_beta @ (a - da)
-            loss = cp.matrix_frac(error, sigma_b) + cp.matrix_frac(da, sigma_a)
-            return loss
-
-        def full_target(b, a, da, sigma_b, sigma_a, lambda_value, underline_beta, y):
-            return qp_target(b, a, da, sigma_b, sigma_a, underline_beta) + lambda_value * cp.norm1(y)
-
-        def is_stationary_point(f_current, f_previous):
-            return np.abs(f_current - f_previous) < self._abs_tol or np.abs(f_current - f_previous) / np.abs(
-                f_previous) < self._rel_tol
-
-        beta_lasso_res = []
-        e_qp_res = []
-        y_errors = []
-        e_errors = []
-        targets = []
+        beta_lasso = None
         for it in tqdm(range(self._max_iterations)):
-            lasso_prob = cp.Problem(cp.Minimize(lasso_target(b, A, dA, sigma_b, self._lambda_value, beta)))
+            lasso_prob = cp.Problem(cp.Minimize(self.lasso_target(b, A, dA, sigma_b, self._lambda, beta)))
             lasso_prob.solve()
 
             beta_lasso = unvectorize_matrix(make_complex_vector(beta.value), (n, n))
-            underline_y = make_real_matrix(np.kron(beta_lasso.T, np.eye(samples)))
+            underline_beta = make_real_matrix(np.kron(beta_lasso.T, np.eye(samples)))
 
-            qp_prob = cp.Problem(cp.Minimize(qp_target(b, a, da, sigma_b, sigma_a, underline_y)))
+            qp_prob = cp.Problem(cp.Minimize(self.qp_target(b, a, da, sigma_b, sigma_a, underline_beta)))
             qp_prob.solve()
 
             e_qp = unvectorize_matrix(make_complex_vector(da.value), x.shape)
             dA = make_real_matrix(np.kron(np.eye(n), e_qp))
 
-            beta_lasso_res.append(beta_lasso)
-            e_qp_res.append(e_qp)
+            target = self.full_target(b, a, da.value, sigma_b, sigma_a, self._lambda, underline_beta, beta.value).value
+            self._iterations.append(IterationStatus(it, beta_lasso, target))
 
-            targets.append(
-                full_target(b, a, da.value, sigma_b, sigma_a, self._lambda_value, underline_y, beta.value).value)
-
-            target_current = targets[it]
-            target_previous = targets[it - 1] if it > 0 else np.inf
-            if is_stationary_point(target_current, target_previous):
+            if it > 0 and self.is_stationary_point(target, self.iteration[it - 1].target_function):
                 break
 
         self._admittance_matrix = beta_lasso
