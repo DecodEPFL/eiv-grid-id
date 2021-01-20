Added D&T matrices, fast covariance inversion, new load profiles and minor bug fixes.

src/models/error_in_variable.py
@@ -38,7 +38,7 @@ class TotalLeastSquares(GridIdentificationModel, UnweightedModel):
 class SparseTotalLeastSquare(GridIdentificationModel, MisfitWeightedModel):
 
     def __init__(self, lambda_value=10e-2, abs_tol=10e-6, rel_tol=10e-6, max_iterations=50,
-                 penalty=1.0, verbose=False, solver=DEFAULT_SOLVER, use_GPU=False):
+                 penalty=1.0, verbose=False, solver=DEFAULT_SOLVER, use_GPU=False, enforce_laplacian=True):
         GridIdentificationModel.__init__(self)
         self._penalty = penalty
         self._l_prior = None
@@ -56,6 +56,7 @@ class SparseTotalLeastSquare(GridIdentificationModel, MisfitWeightedModel):
         self._rel_tol = rel_tol
         self._max_iterations = max_iterations
         self._use_GPU = use_GPU
+        self.enforce_y_cons = enforce_laplacian
 
     #static properties
     LAPLACE = "Laplace"
@@ -117,8 +118,12 @@ class SparseTotalLeastSquare(GridIdentificationModel, MisfitWeightedModel):
 
         #copy data
         samples, n = x.shape
+        DT = duplication_matrix(n) @ transformation_matrix(n)
 
-        A = make_real_matrix(np.kron(np.eye(n), x))
+        if self.enforce_y_cons:
+            A = make_real_matrix(np.kron(np.eye(n), x) @ DT)
+        else:
+            A = make_real_matrix(np.kron(np.eye(n), x))
         dA = np.zeros(A.shape)
         a = make_real_vector(vectorize_matrix(x))
         b = make_real_vector(vectorize_matrix(y))
@@ -133,7 +138,7 @@ class SparseTotalLeastSquare(GridIdentificationModel, MisfitWeightedModel):
             y_weight = sparse.csc_matrix(y_weight)
             x_weight = sparse.csc_matrix(x_weight)
 
-        y = make_real_vector(vectorize_matrix(y_init))
+        y = make_real_vector(vectorize_matrix(y_init) @ DT)
         y_mat = y_init
         z = y
         c = np.zeros(y.shape)
@@ -158,7 +163,10 @@ class SparseTotalLeastSquare(GridIdentificationModel, MisfitWeightedModel):
 
             #create dA from da
             e_qp = unvectorize_matrix(make_complex_vector(da), x.shape)
-            dA = make_real_matrix(np.kron(np.eye(n), e_qp))
+            if self.enforce_y_cons:
+                dA = make_real_matrix(np.kron(np.eye(n), e_qp) @ DT)
+            else:
+                dA = make_real_matrix(np.kron(np.eye(n), e_qp))
 
             #update y
             """
@@ -175,13 +183,24 @@ class SparseTotalLeastSquare(GridIdentificationModel, MisfitWeightedModel):
             c = c + (y - z)
             """
 
-            #z = [(1/i if i > self.cons_multiplier_step_size else 0) for i in np.abs(y)]
+            if self._l_prior_mat is not None:
+                ll = l * self._l_prior_mat
+            else:
+                ll = l * np.eye(y.size)
+            if self._g_prior_mat is not None:
+                lg = l * self._g_prior_mat
+            else:
+                lg = 0 * l * np.eye(y.size)
+
             z = np.divide(1, np.abs(y) + self.cons_multiplier_step_size)
-            #z = np.array([(1/(i + self.cons_multiplier_step_size)) for i in np.abs(y)])
             AmdA = sparse.csc_matrix(A - dA)
-            W = sparse.diags(z)
-            iASA = (AmdA.T @ y_weight @ AmdA) + l * W @ W
+            W = sparse.diags(z)# @ sparse.diags(z)
+            iASA = (AmdA.T @ y_weight @ AmdA) + ll @ W + lg
             ASb_vec = AmdA.T @ y_weight @ b
+            if self._l_prior is not None:
+                ASb_vec = ASb_vec + ll @ W @ self._l_prior
+            if self._g_prior is not None:
+                ASb_vec = ASb_vec + lg @ self._g_prior
 
             if self._use_GPU:
                 y = cuspsolve(iASA, ASb_vec)
@@ -191,7 +210,7 @@ class SparseTotalLeastSquare(GridIdentificationModel, MisfitWeightedModel):
             self.tmp.append(l)
 
             #update cost function
-            y_mat = unvectorize_matrix(make_complex_vector(y), (n,n))
+            y_mat = unvectorize_matrix(DT @ make_complex_vector(y), (n,n))
             target = self._full_target(b, A, da, dA, x_weight, y_weight, y, l)
             self._iterations.append(IterationStatus(it, y_mat, target))
 

src/models/noise_transformation.py
@@ -4,7 +4,7 @@ from scipy import sparse
 from src.models.matrix_operations import vectorize_matrix
 
 
-def exact_noise_covariance(measurement: np.array, sd_magnitude: float, sd_phase: float) -> np.array:
+def exact_noise_covariance(measurement: np.array, sd_magnitude: float, sd_phase: float, inverted: bool = False) -> np.array:
     measurement_vect = vectorize_matrix(measurement)
     m, f = np.abs(measurement_vect), np.angle(measurement_vect)
     m_var, f_var = sd_magnitude ** 2, sd_phase ** 2
@@ -15,6 +15,13 @@ def exact_noise_covariance(measurement: np.array, sd_magnitude: float, sd_phase:
                 np.sin(f) ** 2 * (np.cosh(f_var) - 1) + np.cos(f) ** 2 * np.sinh(f_var)) + m_var * np.exp(-f_var) * (
                            np.sin(f) ** 2 * np.cosh(f_var) + np.cos(f) ** 2 * np.sinh(f_var))
     cov = np.sin(f) * np.cos(f) * np.exp(-2 * f_var) * (m_var + m ** 2 * (1 - np.exp(f_var)))
+
+    if inverted:
+        real_var_inv = np.divide(1, real_var - np.multiply(cov, np.divide(cov, imag_var)))
+        imag_var_inv = np.divide(1, imag_var - np.multiply(cov, np.divide(cov, real_var)))
+        cov_inv = -np.divide(np.multiply(cov, real_var_inv), imag_var)
+        real_var, imag_var, cov = real_var_inv, imag_var_inv, cov_inv
+
     sigma = sparse.bmat([
         [sparse.diags(real_var), sparse.diags(cov)],
         [sparse.diags(cov), sparse.diags(imag_var)]
@@ -65,7 +72,7 @@ def average_true_cov(measurement: np.array, sd_magnitude: float, sd_phase: float
     return real_imag_cov
 
 
-def average_true_noise_covariance(measurement: np.array, sd_magnitude: float, sd_phase: float) -> np.array:
+def average_true_noise_covariance(measurement: np.array, sd_magnitude: float, sd_phase: float, inverted: bool = False) -> np.array:
     if type(sd_magnitude) is not float and sd_magnitude.size == measurement.shape[1]:
         real_variance = np.zeros(measurement.shape)
         imag_variance = np.zeros(measurement.shape)
@@ -84,6 +91,12 @@ def average_true_noise_covariance(measurement: np.array, sd_magnitude: float, sd
         real_variance = average_true_var_real(measurement_vect, sd_magnitude, sd_phase)
         imag_variance = average_true_var_imag(measurement_vect, sd_magnitude, sd_phase)
         real_imag_covariance = average_true_cov(measurement_vect, sd_magnitude, sd_phase)
+
+    if inverted:
+        real_var_inv = np.divide(1, real_variance - np.multiply(real_imag_covariance, np.divide(real_imag_covariance, imag_variance)))
+        imag_var_inv = np.divide(1, imag_variance - np.multiply(real_imag_covariance, np.divide(real_imag_covariance, real_variance)))
+        cov_inv = -np.divide(np.multiply(real_imag_covariance, real_var_inv), imag_variance)
+        real_variance, imag_variance, real_imag_covariance = real_var_inv, imag_var_inv, cov_inv
 
     sigma = sparse.bmat([
         [sparse.diags(real_variance), sparse.diags(real_imag_covariance)],

test/full_test.py
@@ -11,10 +11,14 @@ import scipy as sp
 import cvxpy as cp
 import seaborn as sns
 import mlflow
+from tqdm import tqdm
 
 from scipy import sparse
 from scipy.io import loadmat
+from scipy.interpolate import interp1d
+from scipy.ndimage import convolve1d
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 # %%
 
@@ -29,14 +33,15 @@ import sys
 
 sys.path.insert(1, '..')
 
-from src.models.matrix_operations import make_real_vector, vectorize_matrix
+from src.models.matrix_operations import make_real_vector, vectorize_matrix, duplication_matrix, transformation_matrix
 from src.simulation.noise import add_polar_noise_to_measurement
 from src.models.regression import ComplexRegression, ComplexLasso
 from src.models.error_in_variable import TotalLeastSquares, SparseTotalLeastSquare
-from src.simulation.load_profile import generate_gaussian_load, BusData
-from src.simulation.network import add_load_power_control, make_y_bus, LineData
+from src.simulation.load_profile import generate_gaussian_load
+from src.simulation.network import add_load_power_control, make_y_bus
 from src.simulation.simulation import run_simulation, get_current_and_voltage
-from src.simulation.net_templates import NetData, bolognani_bus21, bolognani_net21, bolognani_bus56, bolognani_net56
+from src.simulation.net_templates import NetData, bolognani_bus21, bolognani_net21, \
+                                         bolognani_bus56, bolognani_net56, bolognani_mapping21
 from src.identification.error_metrics import error_metrics, fro_error
 from src.models.noise_transformation import average_true_noise_covariance, exact_noise_covariance
 from conf.conf import DATA_DIR
@@ -66,11 +71,24 @@ for l in net_data:
 net = NetData(bus_data, net_data)
 
 nodes = len(bus_data)
-steps = 400
-load_cv = 0.5
-current_magnitude_sd = 2e-6
-voltage_magnitude_sd = 2e-6
-phase_sd = 2e-6 / np.pi
+selected_weeks = np.array([0, 5, 10, 15, 20, 25])
+days = 3*len(selected_weeks)
+steps = 10000
+load_cv = 0.0
+current_magnitude_sd = 1e-4
+voltage_magnitude_sd = 1e-4
+phase_sd = 1e-4 / np.pi
+fmeas = 50 # [Hz]
+
+np.random.seed(11)
+DT = duplication_matrix(nodes) @ transformation_matrix(nodes)
+
+# %%
+
+redo_loads = False
+redo_netsim = False
+redo_noise = False
+redo_inverse = False
 
 # %% md
 
@@ -85,26 +103,156 @@ pmu_safety_factor = 4
 pmu_ratings = np.array([pmu_safety_factor*np.sqrt(i.Pd*i.Pd + i.Qd*i.Qd) for i in bus_data])
 pmu_ratings[-1] = np.sum(pmu_ratings) # injection from the grid
 
-# %%
+# %% md
 
-load_profile = loadmat(DATA_DIR / "demandprofile.mat")
-
-np.random.seed(11)
-load_p, load_q = generate_gaussian_load(net.load.p_mw, net.load.q_mvar, load_cv, steps)
-controlled_net = add_load_power_control(net, load_p, load_q)
-sim_result = run_simulation(controlled_net, verbose=False)
-y_bus = make_y_bus(controlled_net)
-voltage, current = get_current_and_voltage(sim_result, y_bus)
-controlled_net.bus
+# Getting profiles as PQ loads every 5 secs for 12 hours.
+# Measurments are taken at 50Hz so we can apply a moving-average low-pass filter.
 
 # %%
+if redo_loads:
+    # print("Reading load profile")
+    # load_profile = loadmat(DATA_DIR / "demandprofile.mat")
+    # n_loads = load_profile['p'].shape[0]
+    # load_profile['p'] = np.c_[load_profile['p'], np.zeros((n_loads, 1))]
+    # load_profile['q'] = np.c_[load_profile['q'], np.zeros((n_loads, 1))]
+    # load_p = np.zeros((n_loads*days, nodes))
+    # load_q = load_p
+    # times = np.zeros((n_loads*days, 1))
+    # ts = np.linspace(0, days*np.max(load_profile['t']), round(np.max(load_profile['t'])*fmeas*days))
+    # for d in range(days):
+    #     for i in range(nodes):
+    #         j = bolognani_mapping21[i] if i in bolognani_mapping21.keys() else i
+    #         times[d*n_loads:((d+1)*n_loads)] = d*np.max(load_profile['t']) + load_profile['t']
+    #         load_p[d*n_loads:((d+1)*n_loads), i] = load_profile['p'][:, j] * np.random.normal(1, load_cv)
+    #         load_q[d*n_loads:((d+1)*n_loads), i] = load_profile['q'][:, j] * np.random.normal(1, load_cv)
+    # print("Done!")
 
-current = np.array(voltage @ y_bus)
-voltage = voltage - np.mean(voltage)
-current = current - np.mean(current)
-noisy_voltage = add_polar_noise_to_measurement(voltage, voltage_magnitude_sd, phase_sd)
-noisy_current = add_polar_noise_to_measurement(current, current_magnitude_sd * pmu_ratings, phase_sd)
-voltage_error, current_error = noisy_voltage - voltage, noisy_current - current
+    print("Reading standard profiles...")
+    pload_profile = None
+    qload_profile = None
+    for d in tqdm(selected_weeks*7):
+        pl = pd.read_csv(DATA_DIR / "profiles/Electricity_Profile.csv", sep=';', header=None, engine='python',
+                           skiprows=d*24*60, skipfooter=round(365*24*60 - (d+days/len(selected_weeks))*24*60)).to_numpy()
+        pload_profile = pl if pload_profile is None else np.vstack((pload_profile, pl))
+
+    for d in tqdm(selected_weeks*7):
+        ql = pd.read_csv(DATA_DIR / "profiles/Reactive_Electricity_Profile.csv", sep=';', header=None, engine='python',
+                           skiprows=d*24*60, skipfooter=round(365*24*60 - (d+days/len(selected_weeks))*24*60)).to_numpy()
+        qload_profile = ql if qload_profile is None else np.vstack((qload_profile, ql))
+
+    pload_profile = pload_profile/1e6 # [MW]
+    qload_profile = qload_profile/1e6 # [MVA]
+    p_mean_percentile = np.mean(np.percentile(np.abs(pload_profile), 90, axis=0))
+    q_mean_percentile = np.mean(np.percentile(np.abs(qload_profile), 90, axis=0))
+
+    times = np.array(range(days*24*60))*60 #[s]
+    print("Done!")
+
+    print("Assigning random households to nodes...")
+    load_p = np.zeros((pload_profile.shape[0], nodes))
+    load_q = np.zeros((qload_profile.shape[0], nodes))
+    for i in tqdm(range(nodes)):
+        load_p[:, i] = np.sum(pload_profile[:, np.random.randint(pload_profile.shape[1], size=round(net.load.p_mw[i]/p_mean_percentile))], axis=1)
+        load_q[:, i] = np.sum(qload_profile[:, np.random.randint(qload_profile.shape[1], size=round(net.load.q_mvar[i]/q_mean_percentile))], axis=1)
+    print("Done!")
+
+    print("Saving loads...")
+    sim_PQ = {'p': load_p, 'q': load_q, 't': times}
+    np.savez(DATA_DIR / "simulations_output/sim_loads.npz", **sim_PQ)
+    print("Done!")
+
+if redo_netsim:
+    print("Loading loads...")
+    sim_PQ = np.load(DATA_DIR / "simulations_output/sim_loads.npz")
+    load_p = sim_PQ["p"]
+    load_q = sim_PQ["q"]
+    times = sim_PQ["t"]
+    print("Done!")
+
+    mpl.rcParams['lines.linewidth'] = 1
+    for i in range(nodes):
+        tmpplt = pd.Series(load_p[0:60*24, i]).plot()
+    plt_tmp = tmpplt.get_figure()
+    plt_tmp.savefig(DATA_DIR / 'loads.pdf')
+    plt.clf()
+
+    # for cubic interpolation, we need to do it on the loads, but it's very slow !
+    # print("Interpolating missing loads...")
+    # f = interp1d(load_profile['t'].squeeze(), load_p, axis=0, type="cubic")
+    # load_p = f(ts)
+    # f = interp1d(load_profile['t'].squeeze(), load_q, axis=0, type="cubic")
+    # load_q = f(ts)
+    # print("Done!")
+
+
+    print("Simulating network...")
+    #load_p, load_q = generate_gaussian_load(net.load.p_mw, net.load.q_mvar, load_cv, steps)
+    controlled_net = add_load_power_control(net, load_p, load_q)
+    sim_result = run_simulation(controlled_net, verbose=True)
+    y_bus = make_y_bus(controlled_net)
+    voltage, current = get_current_and_voltage(sim_result, y_bus)
+    controlled_net.bus
+
+    # linear interpolation of missing timesteps
+    ts = np.linspace(0, np.max(times), round(np.max(times)*fmeas))
+    f = interp1d(times.squeeze(), voltage, axis=0)
+    voltage = f(ts)
+    f = interp1d(times.squeeze(), current, axis=0)
+    current = f(ts)
+    print("Done!")
+
+
+
+    print("Saving data...")
+    sim_IV = {'i': current, 'v': voltage, 'y': y_bus}
+    np.savez(DATA_DIR / "simulations_output/sim_results.npz", **sim_IV)
+    print("Done!")
+
+if redo_noise:
+    print("Loading data...")
+    sim_IV = np.load(DATA_DIR / "simulations_output/sim_results.npz")
+    voltage = sim_IV["v"]
+    current = sim_IV["i"]
+    y_bus = sim_IV["y"]
+    print("Done!")
+
+    # %%
+
+    print("Adding noise...")
+    current = np.array(voltage @ y_bus)
+    voltage = voltage - np.mean(voltage)
+    current = current# - np.mean(current)
+    noisy_voltage = add_polar_noise_to_measurement(voltage, voltage_magnitude_sd, phase_sd)
+    noisy_current = add_polar_noise_to_measurement(current, current_magnitude_sd * pmu_ratings, phase_sd)
+    print("Done!")
+
+    print("Filtering data...")
+    fparam = int(np.floor(voltage.shape[0]/steps))
+
+    tmp_voltage = 1j*np.zeros((steps,nodes))
+    tmp_current = 1j*np.zeros((steps,nodes))
+    for i in range(steps):
+        tmp_voltage[i] = np.sum(noisy_voltage[i*fparam:(i+1)*fparam], axis=0)/fparam
+        tmp_current[i] = np.sum(noisy_current[i*fparam:(i+1)*fparam], axis=0)/fparam
+
+    noisy_voltage = tmp_voltage
+    noisy_current = tmp_current
+    print("Done!")
+
+
+    print("Saving filtered data...")
+    sim_IV = {'i': noisy_current, 'v': noisy_voltage, 'y': y_bus}
+    np.savez(DATA_DIR / "simulations_output/filtered_results.npz", **sim_IV)
+    print("Done!")
+
+
+
+print("Loading filtered data...")
+sim_IV = np.load(DATA_DIR / "simulations_output/filtered_results.npz")
+noisy_voltage = sim_IV["v"]
+noisy_current = sim_IV["i"]
+y_bus = sim_IV["y"]
+print("Done!")
 
 # %%
 
@@ -134,6 +282,7 @@ mlflow_params = {
 
 # %%
 
+print("TLS identification...")
 with mlflow.start_run(run_name='TLS'):
     tls = TotalLeastSquares()
     tls.fit(noisy_voltage, noisy_current)
@@ -147,6 +296,7 @@ sns_plot = sns.heatmap(np.abs(y_tls))
 fig_t = sns_plot.get_figure()
 fig_t.savefig(DATA_DIR / "y_tls.png")
 plt.clf()
+print("Done!")
 
 # %% md
 
@@ -156,44 +306,55 @@ plt.clf()
 # currents
 
 # %%
-
 with mlflow.start_run(run_name='S-TLS with covariance'):
-    max_iterations = 10000
-    abs_tol = 1e3
+    max_iterations = 50
+    abs_tol = 1e-1
     rel_tol = 10e-8
     solver = cp.GUROBI
     use_cov_matrix = True
     pen_degree = 1.0
-    tls_weights_adaptive = np.divide(1.0, np.power(np.abs(make_real_vector(vectorize_matrix(y_tls))), 1.0))
+    tls_weights_adaptive = np.divide(1.0, np.power(np.abs(make_real_vector(vectorize_matrix(y_tls) @ DT)), 1.0))
+    tls_weights_tridiag = make_real_vector((1+1j)*vectorize_matrix(np.diag(np.ones(nodes)) +
+                                                                   np.diag(np.ones(nodes-1), k=1) +
+                                                                   np.diag(np.ones(nodes-1), k=-1)) @ DT)
+    tls_weights_all = np.multiply(tls_weights_adaptive, tls_weights_tridiag)
 
-    sigma_voltage = average_true_noise_covariance(noisy_voltage, voltage_magnitude_sd, phase_sd)
-    sigma_current = average_true_noise_covariance(noisy_current, current_magnitude_sd * pmu_ratings, phase_sd)
+    # sigma_voltage = average_true_noise_covariance(noisy_voltage, voltage_magnitude_sd, phase_sd)
+    # sigma_current = average_true_noise_covariance(noisy_current, current_magnitude_sd * pmu_ratings, phase_sd)
 
     # sigma_voltage = exact_noise_covariance(voltage, voltage_magnitude_sd, phase_sd)
     # sigma_current = exact_noise_covariance(current, current_magnitude_sd, phase_sd)
 
-    print("Starting matrix inversion...")
+    if redo_inverse:
+        print("Calculating covariance matrices...")
+        inv_sigma_voltage = average_true_noise_covariance(noisy_voltage, voltage_magnitude_sd, phase_sd, True)
+        inv_sigma_current = average_true_noise_covariance(noisy_current, current_magnitude_sd * pmu_ratings, phase_sd, True)
+        print("Done!")
 
-    if max_iterations > 0:
-        inv_sigma_current = sparse.linalg.inv(sigma_current)#/1e8
-        inv_sigma_voltage = sparse.linalg.inv(sigma_voltage)#/1e8
+        print("Saving matrix inverse...")
+        sparse.save_npz(DATA_DIR / "simulations_output/inverse_cov_I.npz", inv_sigma_current)
+        sparse.save_npz(DATA_DIR / "simulations_output/inverse_cov_V.npz", inv_sigma_voltage)
+        print("Done!")
     else:
-        inv_sigma_current = sigma_current
-        inv_sigma_voltage = sigma_voltage
+        print("Loading saved matrix...")
+        inv_sigma_current = sparse.load_npz(DATA_DIR / "simulations_output/inverse_cov_I.npz")
+        inv_sigma_voltage = sparse.load_npz(DATA_DIR / "simulations_output/inverse_cov_V.npz")
+        print("Done!")
 
-    print("Done!")
-
-    sparse_tls_cov = SparseTotalLeastSquare(lambda_value=1e8, abs_tol=abs_tol, rel_tol=rel_tol, solver=solver,
+    print("STLS identification...")
+    sparse_tls_cov = SparseTotalLeastSquare(lambda_value=1e6, abs_tol=abs_tol, rel_tol=rel_tol, solver=solver,
                                             max_iterations=max_iterations, use_GPU=True)
-    sparse_tls_cov.l1_multiplier_step_size = 2#0.5
-    sparse_tls_cov.cons_multiplier_step_size = 0.001
-    # sparse_tls_cov.set_prior(make_real_vector(vectorize_matrix(np.zeros(y_tls.shape))), SparseTotalLeastSquare.LAPLACE,
-    #                         np.diag(tls_weights_adaptive))
+    sparse_tls_cov.l1_multiplier_step_size = 20#0.2*0.000001#2#0.02
+    sparse_tls_cov.cons_multiplier_step_size = 0.00001
+    # sparse_tls_cov.set_prior(make_real_vector(vectorize_matrix(np.zeros(y_tls.shape)) @ DT),
+    #                          SparseTotalLeastSquare.LAPLACE, np.diag(tls_weights_adaptive))
+    sparse_tls_cov.set_prior(make_real_vector(vectorize_matrix(np.zeros(y_tls.shape)) @ DT),
+                             SparseTotalLeastSquare.LAPLACE, np.diag(tls_weights_all))
 
     #sparse_tls_cov.l1_target = 2 * np.count_nonzero(y_bus)
     #sparse_tls_cov.l1_target = 1.0 * (np.sum(np.power(np.abs(np.real(y_bus)), pen_degree))
     #                                  + np.sum(np.power(np.abs(np.imag(y_bus)), pen_degree)))
-    sparse_tls_cov.l1_target = 1.0 * 2 * np.sum(np.abs(make_real_vector(np.diag(y_tls))))
+    sparse_tls_cov.l1_target = 1.0 * 0.5 * np.sum(np.abs(make_real_vector(np.diag(y_tls))))
     print(sparse_tls_cov.l1_target)
     print(np.sum(np.abs(make_real_vector(vectorize_matrix(y_bus)))))
 
@@ -204,6 +365,7 @@ with mlflow.start_run(run_name='S-TLS with covariance'):
 
     sparse_tls_cov_errors = pd.Series([fro_error(y_bus, i.fitted_parameters) for i in sparse_tls_cov.iterations])
     sparse_tls_cov_targets = pd.Series([i.target_function for i in sparse_tls_cov.iterations])
+    sparse_tls_cov_multipliers = pd.Series(sparse_tls_cov.tmp)
 
     mlflow.log_param('max_iterations', max_iterations)
     mlflow.log_param('abs_tol', abs_tol)
@@ -216,6 +378,7 @@ with mlflow.start_run(run_name='S-TLS with covariance'):
     for i in range(len(sparse_tls_cov_errors)):
         mlflow.log_metric('fro_error_evo', value=sparse_tls_cov_errors[i], step=i)
         mlflow.log_metric('opt_cost_evo', value=sparse_tls_cov_targets[i], step=i)
+    print("Done!")
 
 # %%
 
@@ -277,3 +440,9 @@ stcplt = sparse_tls_cov_targets.plot()
 plt_stc = stcplt.get_figure()
 plt_stc.savefig(DATA_DIR / 'targets.pdf')
 plt.clf()
+stcplt = sparse_tls_cov_multipliers.plot()
+plt_stc = stcplt.get_figure()
+plt_stc.savefig(DATA_DIR / 'multipliers.pdf')
+plt.clf()
+
+