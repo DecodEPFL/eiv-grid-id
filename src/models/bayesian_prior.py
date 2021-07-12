Bugfix for sign on exact prior and prior params shapes.

src/models/bayesian_prior.py
@@ -23,7 +23,7 @@ class BayesianPrior(object):
 
     def __init__(self, n=0, other=None):
         self.L = other.L if other is not None else np.zeros((0,n))
-        self.mu = other.mu if other is not None else np.zeros((0,1))
+        self.mu = other.mu if other is not None else np.zeros(0)
         self.n = n
 
     def _std_weights(self, weights, standard):
@@ -44,10 +44,10 @@ class BayesianPrior(object):
         """
         weights = self._std_weights(weights, values)
 
-        self.mu = np.vstack((self.mu, np.reshape(weights,(len(indices), 1))))
+        self.mu = np.concatenate((self.mu, weights))
         added_L = np.zeros((len(indices), self.n))
         for i in range(len(indices)):
-            added_L[i, indices[i]] = -weights[i]/values[i]
+            added_L[i, indices[i]] = weights[i]/values[i]
 
         self.L = np.vstack((self.L, added_L))
 
@@ -58,9 +58,9 @@ class BayesianPrior(object):
         :param indices: indices of the parameters on which the prior apply
         :param weights: uncertainty on the significance of the parameter: high if weight is low
         """
-        weights = self._std_weights(weights, np.array(indices))
+        weights = self._std_weights(weights, np.array(indices, dtype=self.mu.dtype))
 
-        self.mu = np.vstack((self.mu, np.zeros((len(indices),1))))
+        self.mu = np.concatenate((self.mu, np.zeros(len(indices))))
         added_L = np.zeros((len(indices), self.n))
         for i in range(len(indices)):
             added_L[i, indices[i]] = weights[i]
@@ -75,7 +75,7 @@ class BayesianPrior(object):
         :param indices: indices of the parameters on which the prior apply
         :param values: values of the parameters for corresponding indices
         """
-        self.mu = np.vstack((self.mu, np.zeros((len(indices),1))))
+        self.mu = np.concatenate((self.mu, np.zeros(len(indices))))
         added_L = np.zeros((len(indices), self.n))
         for i in range(len(indices)):
             added_L[i, indices[i]] = 1.0/values[i]
@@ -92,7 +92,7 @@ class BayesianPrior(object):
         """
         weights = self._std_weights(weights, values)
 
-        self.mu = np.vstack((self.mu, weights))
+        self.mu = np.concatenate((self.mu, weights))
         added_L = np.zeros((indices.shape[0], self.n))
         for i in range(indices.shape[0]):
             added_L[i, indices[i, :]] = weights[i] / values[i]
@@ -108,9 +108,9 @@ class BayesianPrior(object):
         :param weights: weights for summing each row
         """
         assert(indices.shape[1] >= 2)
-        weights = self._std_weights(weights, np.array(indices[:, 0]))
+        weights = self._std_weights(weights, np.array(indices[:, 0], dtype=self.mu.dtype))
 
-        self.mu = np.vstack((self.mu, np.zeros((len(indices),1))))
+        self.mu = np.concatenate((self.mu, np.zeros(len(indices))))
         added_L = np.zeros((indices.shape[0], self.n))
         for i in range(indices.shape[0]):
             added_L[i, indices[i, :]] = np.array([1, -values[i]])*weights[i]

src/models/smooth_prior.py
@@ -38,7 +38,7 @@ class SmoothPrior(BayesianPrior):
     GAUSS = 2
 
     def log_distribution(self, x):
-        w = (np.abs(self.L @ x.squeeze() - self.mu.squeeze()) ** (2 - self.gamma.squeeze())) + self.alpha
+        w = (np.abs(self.L @ x.squeeze() - self.mu) ** (2 - self.gamma)) + self.alpha
 
         A = self.L.T @ np.diag(np.divide(1, w)) @ self.L
         b = self.L.T @ np.diag(np.divide(1, w)) @ self.mu
@@ -56,8 +56,8 @@ class SmoothPrior(BayesianPrior):
         """
         BayesianPrior.add_exact_prior(self, indices, values, weights)
 
-        orders = np.reshape(self._std_weights(orders, values), (len(indices), 1))
-        self.gamma = np.vstack((self.gamma, orders))
+        orders = self._std_weights(orders, values)
+        self.gamma = np.concatenate((self.gamma, orders))
 
     def add_sparsity_prior(self, indices, weights=None, orders=2):
         """
@@ -69,8 +69,8 @@ class SmoothPrior(BayesianPrior):
         """
         BayesianPrior.add_sparsity_prior(self, indices, weights)
 
-        orders = np.reshape(self._std_weights(orders, np.array(indices)), (len(indices), 1))
-        self.gamma = np.vstack((self.gamma, orders))
+        orders = self._std_weights(orders, np.array(indices))
+        self.gamma = np.concatenate((self.gamma, orders))
 
     def add_adaptive_sparsity_prior(self, indices, values, orders=2):
         """
@@ -83,8 +83,8 @@ class SmoothPrior(BayesianPrior):
         """
         BayesianPrior.add_adaptive_sparsity_prior(self, indices, values)
 
-        orders = np.reshape(self._std_weights(orders, values), (len(indices), 1))
-        self.gamma = np.vstack((self.gamma, orders))
+        orders = self._std_weights(orders, values)
+        self.gamma = np.concatenate((self.gamma, orders))
 
     def add_contrast_prior(self, indices, values, weights=None, orders=2):
         """
@@ -97,8 +97,8 @@ class SmoothPrior(BayesianPrior):
         """
         BayesianPrior.add_contrast_prior(self, indices, values, weights)
 
-        orders = np.reshape(self._std_weights(orders, values), (len(indices), 1))
-        self.gamma = np.vstack((self.gamma, orders))
+        orders = self._std_weights(orders, values)
+        self.gamma = np.concatenate((self.gamma, orders))
 
     def add_ratios_prior(self, indices, values, weights=None, orders=2):
         """
@@ -111,8 +111,8 @@ class SmoothPrior(BayesianPrior):
         """
         BayesianPrior.add_ratios_prior(self, indices, values, weights)
 
-        orders = np.reshape(self._std_weights(orders, values), (len(indices), 1))
-        self.gamma = np.vstack((self.gamma, orders))
+        orders = self._std_weights(orders, values)
+        self.gamma = np.concatenate((self.gamma, orders))
 
 
 class SparseSmoothPrior(SmoothPrior):
@@ -125,7 +125,7 @@ class SparseSmoothPrior(SmoothPrior):
         self.L = sparse.csr_matrix(self.L)
 
     def log_distribution(self, x):
-        w = (np.abs(self.L @ x.squeeze() - self.mu.squeeze()) ** (2 - self.gamma.squeeze())) + self.alpha
+        w = (np.abs(self.L @ x.squeeze() - self.mu) ** (2 - self.gamma)) + self.alpha
         W = sparse.diags(np.divide(1, w), format='csr')
 
         A = self.L.T @ W @ self.L

test/full_test.py
@@ -396,7 +396,7 @@ if redo_standard_methods:
     y_sym_ols = unvectorize_matrix(DT @ E @ vectorize_matrix(y_ols), (newnodes, newnodes))
     adaptive_lasso = np.divide(1.0, np.power(np.abs(make_real_vector(E @ vectorize_matrix(y_sym_ols))), 1.0))
     prior = SparseSmoothPrior(smoothness_param=0.00001, n=len(E @ vectorize_matrix(y_tls))*2)
-    prior.add_adaptive_sparsity_prior(np.arange(prior.n), adaptive_lasso.reshape((prior.n, 1)), SmoothPrior.LAPLACE)
+    prior.add_adaptive_sparsity_prior(np.arange(prior.n), adaptive_lasso, SmoothPrior.LAPLACE)
 
     if use_laplacian:
         lasso = BayesianRegression(prior, lambda_value=1e-7, abs_tol=abs_tol, rel_tol=rel_tol, max_iterations=10,
@@ -494,7 +494,6 @@ if (not constant_power_hidden_nodes and observed_nodes != list(range(1, 57))) or
 else:
     values_contrast = -np.concatenate((np.real(np.sum(y_sym_tls_ns, axis=1)), np.imag(np.sum(y_sym_tls_ns, axis=1))))
 values_contrast = values_contrast if use_laplacian else -values_contrast
-values_contrast = values_contrast.reshape((len(values_contrast), 1))
 
 if contrast_each_row:
     # Indices of the real part of each rows stacked with the indices of the imaginary part of each row
@@ -509,7 +508,7 @@ if contrast_each_row:
 
     prior.add_contrast_prior(indices=idx_contrast,
                              values=values_contrast,
-                             weights=lambdaprime * np.concatenate((-np.ones((newnodes, 1)), np.ones((newnodes, 1)))),
+                             weights=lambdaprime * np.concatenate((-np.ones(newnodes), np.ones(newnodes))),
                              orders=SmoothPrior.LAPLACE)
 
     if contrast_diag:
@@ -522,8 +521,8 @@ if contrast_each_row:
 
 else:
     prior.add_contrast_prior(indices=np.vstack(tuple(np.split(idx_offdiag, 2))),
-                             values=-np.sum(np.vstack(tuple(np.hsplit(values_contrast.T, 2))), axis=1).T,
-                             weights=lambdaprime*np.array([[1], [-1]]),
+                             values=-np.sum(np.vstack(tuple(np.split(values_contrast, 2))), axis=1).squeeze(),
+                             weights=lambdaprime*np.array([1, -1]),
                              orders=SmoothPrior.LAPLACE)
 
 # %% md