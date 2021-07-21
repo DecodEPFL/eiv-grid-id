8aacbdac9dd7aa86512a282be6737a3ec8ec633f
jbrouill <jean-sebastien.brouillon@epfl.ch>
2021-07-21 18:56:35 +0200

    Tested also reduced sim.

--- conf/simulation.py
+++ conf/simulation.py
@@ -2,7 +2,7 @@ import numpy as np
 
 observed_nodes = [1, 3, 4, 6, 8, 9, 10, 12, 15, 16, 17, 18, 19, 22, 24, 26, 28,
                   32, 36, 37, 39, 40, 43, 44, 46, 47, 49, 50, 51, 52, 53, 55]  # About 60% of the nodes
-observed_nodes = list(range(1, 57))
+#observed_nodes = list(range(1, 57))
 
 hidden_nodes = list(set(range(56)) - set(np.array(observed_nodes)-1))
 constant_load_hidden_nodes = False
@@ -19,5 +19,5 @@ measurement_frequency = 100 # [Hz] # 50
 safety_factor = 4
 
 #for 3ph
-days = len(selected_weeks)*30
-hidden_nodes = []
+#days = len(selected_weeks)
+#hidden_nodes = []
